db3347b9ee9fb038272a0a6c6db652713d7a6a9f
Scott Morrison <scott@tqft.net>
2014-02-09 16:18:27 +1100

    initial setup; noop JSON server, ready for Heroku

---
+++ build.sbt
@@ -0,0 +1,39 @@
+import com.typesafe.startscript.StartScriptPlugin
+
+seq(StartScriptPlugin.startScriptForClassesSettings: _*)
+
+name := "arxiv-match"
+
+organization := "net.tqft"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.10.3"
+
+scalacOptions += "-target:jvm-1.7"
+
+resolvers ++= Seq(
+	"Java.net Maven2 Repository" at "http://download.java.net/maven/2/",
+	"Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases",
+	"Scala Snapshots" at "http://scala-tools.org/repo-snapshots/",
+        "twitter-repo" at "http://maven.twttr.com"
+)
+
+// Project dependencies
+libraryDependencies ++= Seq(
+        "com.twitter" %% "finagle-core" % "6.1.0",
+        "com.twitter" %% "finagle-http" % "6.1.0",
+        "com.github.theon" %% "scala-uri" % "0.3.4",
+	"org.apache.commons" % "commons-lang3" % "3.2.1",
+        "mysql" % "mysql-connector-java" % "5.1.24",
+        "com.typesafe.slick" %% "slick" % "2.0.0-RC1"
+)
+
+// Test dependencies
+libraryDependencies ++= Seq(
+	"junit" % "junit" % "4.8" % "test",
+	"org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+)
+
+publishTo := Some(Resolver.sftp("tqft.net", "tqft.net", "tqft.net/releases") as ("scottmorrison", new java.io.File("/Users/scott/.ssh/id_rsa")))
+
---
+++ src/main/scala/net/tqft/arxivmatch/Web.scala
@@ -0,0 +1,52 @@
+package net.tqft.arxivmatch
+
+import org.jboss.netty.handler.codec.http.{ HttpRequest, HttpResponse }
+import com.twitter.finagle.builder.ServerBuilder
+import com.twitter.finagle.http.{ Http, Response }
+import com.twitter.finagle.Service
+import com.twitter.util.Future
+import java.net.InetSocketAddress
+import util.Properties
+import org.jboss.netty.handler.codec.http.QueryStringDecoder
+import scala.io.Source
+import scala.collection.JavaConverters.asScalaBufferConverter
+
+object Web {
+  def main(args: Array[String]) {
+    val port = Properties.envOrElse("PORT", "8080").toInt
+    println("Starting on port:" + port)
+    ServerBuilder()
+      .codec(Http())
+      .name("arxivmatch")
+      .bindTo(new InetSocketAddress(port))
+      .build(new ResolverService)
+    println("Started.")
+  }
+}
+
+class ResolverService extends Service[HttpRequest, HttpResponse] {
+  def apply(req: HttpRequest): Future[HttpResponse] = {
+    val response = Response()
+
+    val parameters = new QueryStringDecoder(req.getUri()).getParameters
+    import scala.collection.JavaConverters._
+    val callback = Option(parameters.get("callback")).map(_.asScala.headOption).flatten
+
+    val next = None
+    
+    response.setStatusCode(200)
+    val json = next.map({ "{}" })
+    callback match {
+      case Some(c) => {
+        response.setContentType("application/javascript")
+        response.contentString = c + "(" + json + ");"
+      }
+      case None => {
+        response.setContentType("application/json")
+        response.contentString = json
+      }
+    }
+
+    Future(response)
+  }
+}
